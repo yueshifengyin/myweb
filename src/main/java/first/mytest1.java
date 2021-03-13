d441e00f41beba0c39843440a31ff3bf0b5bc244
李佳良 <1272123175@qq.com>
2021-03-13 16:14:27 +0800

    第一次上传

---
+++ src/main/java/first/mytest1.java
@@ -0,0 +1,14 @@
+package first;
+
+public class mytest1 {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
